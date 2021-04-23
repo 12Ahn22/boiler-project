e5dca026c8a42d23f991c4548873d30661ef2188
12Ahn22 <44540726+12Ahn22@users.noreply.github.com>
2021-04-23 15:01:35 +0900

    login Route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'; // package.json에서 type을 module로 설정
 import mongoose from 'mongoose'; // mongoose를 이용해 mongoDB와 app 연결
 import { User } from './models/User.js'; // 만든 User 모델 가져오기
 import { config } from './config/key.js'; //
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 5000;
@@ -11,6 +12,8 @@ const port = 5000;
 app.use(express.urlencoded({ extended: true }));
 // application/json 타입의 데이터를 분석
 app.use(express.json());
+// 쿠키파서 사용하기
+app.use(cookieParser());
 
 mongoose
 	.connect(config.mongoURI, {
@@ -21,25 +24,69 @@ mongoose
 	}) // Promise를 반환
 	.then(() => console.log('MongoDB connected'))
 	.catch((error) => console.log(error));
-
+//
+//
+//
 // 라우트
 app.get('/', (req, res) => {
 	res.send('Hello World!~~');
 });
+
 // 회원 가입용 라우트
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 	// 회원 가입할 떄 필요한 정보들을 client에서 가져오면
 	// 이것들을 데이터 베이스에 넣기
 
 	// req.body에는 json 형식으로 데이터가 들어있다 = body-parser가 있기 때문에 가능
-	const user = new User(req.body);
+	const user = new User(req.body); // user 모델에 req.body값을 넣은 상태
 
 	// save() : mongoDB의 메서드, 정보를 유저 모델에 저장시킨다
-	user.save((err, userInfo) => {
+	user.save((err) => {
 		if (err) return res.json({ success: false, err }); // 실패한 경우, 에러 메세지를 json으로 전달
 		return res.status(200).json({ success: true });
 	});
 });
+
+// 로그인용 라우트
+app.post('/api/users/login', (req, res) => {
+	// DB에서 요청된 이메일 주소 찾기
+	User.findOne({ email: req.body.email }, (err, user) => {
+		// 유저가 없다면
+		if (!user) {
+			return res.json({
+				loginSuccess: false,
+				message: '제공된 Email에 해당하는 유저가 없습니다',
+			});
+		}
+		// DB에 이메일이 있다면 비밀번호가 맞는지 확인하기
+		// UserModel에서 메소드 만들기
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			// isMatch가 null이면 비밀번호가 틀린 것
+			if (!isMatch) {
+				return res.json({
+					loginSuccess: false,
+					message: '비밀번호가 일치하지않습니다',
+				});
+			}
+			// 비밀번호까지 같다면 Token 생성하기
+			// UserModel에서 메소드 만들기
+			user.generateToken((err, user) => {
+				// err가 있다면
+				if (err) return res.status(400).send(err);
+
+				// 클라이언트에 토큰을 저장한다.
+				// 저장가능한 위치 : 쿠키, 로컬스토리지, 세션 스토리지
+				// 여기에선 쿠키 - 쿠키 파서 설치
+				res
+					.cookie('x_auth', user.token)
+					.status(200)
+					.json({ loginSuccess: true, userId: user._id });
+			});
+		});
+	});
+});
+
+app.post('');
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // 몽구스를 이용해서 스키마 작성
 const userSchema = mongoose.Schema({
@@ -31,6 +33,61 @@ const userSchema = mongoose.Schema({
 		type: Number,
 	},
 });
+
+// 암호화
+const saltRound = 10; // salt의 글자수
+// 몽구스 메소드 pre(). 지정한 '함수'가 실행되기 전에 실행되는 함수
+userSchema.pre('save', function (next) {
+	const user = this; // this는 userSchema를 가리킨다
+
+	// 비밀번호를 암호화 시킨다 = 단, 비밀번호를 변경할 때만
+	if (user.isModified('password')) {
+		bcrypt.genSalt(saltRound, (err, salt) => {
+			if (err) return next(err); // 에러가 난 경우 err를 보냄
+
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				if (err) return next(err);
+				// hash는 암호화된 비밀번호
+				// user의 password를 plain text에서 암호화된 해시값으로 변경해준다
+				user.password = hash;
+				// next를 실행하면 save()를 진행하러 간다
+				next();
+			});
+		});
+	} else {
+		// 다른 걸 변경하는 경우 next로 바로 save를 하러 가도록한다
+		next();
+	}
+});
+
+// 비밀번호 맞는지 확인하는 메서드
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+	// plainPassword : 클라이언트가 입력한 패스워드 = plain text
+	// plainPassword을 암호화해서 DB에있는 password와 같은지 확인해야한다
+	// bcrypt.compare(플레인텍스트, 해시, 콜백(에러,성공결과){} )
+
+	bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+		if (err) return callback(err);
+		callback(null, isMatch);
+	});
+};
+
+// Token을 생성하는 메서드
+userSchema.methods.generateToken = function (callback) {
+	const user = this;
+	// jwt을 이용해서 토큰을 생성
+	const token = jwt.sign(user._id.toHexString(), 'secretToken'); // DB에 저장되어있는 _id 값을 가져와 토큰을 생성한다
+	// token = user._id + 'secretToken'
+	// token + 'secretToken' => user._id 토큰과 문자를 통해 user._id값을 얻을 수 있다
+
+	user.token = token;
+	user.save((err, user) => {
+		// 에러가 있다면 콜백에 err전달
+		if (err) return callback(err);
+		// 에러가 없다면 토큰값이 저장된 user정보 전달
+		callback(null, user);
+	});
+};
 
 // 모델로 스키마를 감싼다. (모델명, 스키마)
 export const User = mongoose.model('User', userSchema);
